6e2932650cc0eb78bd2c6651f71fa6e2f544142a
Dimitar Mihaylov <slimarcho@Mac.local>
2020-10-17 19:17:28 +0100

    m

--- tetris/View.js
+++ tetris/View.js
@@ -1,36 +1,9 @@
 const model = require('../tetris/Model.js')
 const discord = require('discord.js')
 
-async function draw(channel, board, player_1, player_2, msg){
-    if(typeof msg === 'undefined'){
-        let str = ( '|                                                            |\n' + 
-                    '|                                                            |\n' + 
-                    '|                                                            |\n' + 
-                    '|                                                            |\n' + 
-                    '|                                                            |\n' + 
-                    '|                                                            |\n' + 
-                    '|                                                            |\n' + 
-                    '|                                                            |\n' + 
-                    '|                                                            |\n' + 
-                    '|                                                            |\n' + 
-                    '|                                                            |\n' + 
-                    '|                                                            |\n' + 
-                    '|                                                            |\n' + 
-                    '|                                                            |\n' + 
-                    '|                                                            |\n' + 
-                    '|                                                            |\n' + 
-                    '|                                                            |\n' + 
-                    '|                                                            |\n' + 
-                    '|                                                            |\n' + 
-                    '|                                                            |\n' + 
-                    '|                                                            |\n' + 
-                    '---------------------------------------\n\n' +
-            ':arrow_left:                                                  :arrow_right:');
-        return await channel.send(str);
-    }
-   else{
-        let str = ('');
-        
+
+function create_string(board){
+    let str = ('');
         for(var i = 0; i < board.length; i++){
             str = str.concat('|')
             for(var j = 0; j < board[i].length; j++){
@@ -39,31 +12,38 @@ async function draw(channel, board, player_1, player_2, msg){
                         str = str.concat('       ')
                         break;
                     case 1:
-                        str = str.concat(' :red_square:')
+                        str = str.concat(':red_square: ')
                         break;
                     case 2:
-                        str = str.concat(' :orange_square:')
+                        str = str.concat(':orange_square: ')
                         break;
                     case 3:
-                        str = str.concat(' :yellow_square:')
+                        str = str.concat(':yellow_square: ')
                         break;
                     case 4:
-                        str = str.concat(' :green_square:')
+                        str = str.concat(':green_square: ')
                         break;
                     case 5:
-                        str = str.concat(' :blue_square:')
+                        str = str.concat(':blue_square: ')
                         break;
                     case 6:
-                        str = str.concat(' :purple_square:')
+                        str = str.concat(':purple_square: ')
                         break;    
                 }
             }
             str = str.concat('|\n')
         }
         str = str.concat('-------------------------------------------\n\n' +
-        ':arrow_left:                                                  :arrow_right:');
-        msg.edit(str);
-    }
-    return msg;   
+        ':arrow_left:                                                  :arrow_right:')
+
+    return str
+}
+async function draw(channel, board, player_1, player_2, msg){
+
+    if(typeof msg === "undefined")
+        return channel.send("Loading board... ")
+    else
+        msg.edit(create_string(board));
+        return msg;   
 }
 exports.draw = draw;
