d93505c2da1766b1f0a69492b661f1f87517dd83
Valts Blezurs <cpb16163@uni.strath.ac.uk>
2020-10-17 15:18:17 +0300

    bot functionality

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, MessageReaction } = require('discord.js');
+const { Client } = require('discord.js');
 const settings = require('./settings.js');
 
 const bot = new Client();
@@ -12,7 +12,16 @@ bot.on('message', msg => {
     if (command === "challenge") {
         msg.channel.send(`<@${msg.author.id}> has issued a challenge to retro pong.`).then(messageReaction => {
             messageReaction.react("👍");
-        })
+
+            const filter = (reaction, user) => {
+                return ['👍'].includes(reaction.emoji.name) && user.id !== messageReaction.author.id && user.id !== msg.author.id;
+            };
+
+            // messageReaction.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+            //     .then(collected => {
+            //         msg.channel.send(`${collected.user} has accepted the challenge from <@${msg.author.id}>.`);
+            //     });
+        });
 
     }
 });
