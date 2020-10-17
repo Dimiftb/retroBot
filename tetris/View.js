91d72e3ef3672a572d0a1a67362a4fa45e68f0e0
Dimitar Mihaylov <slimarcho@Mac.local>
2020-10-17 18:59:04 +0100

    m

--- tetris/View.js
+++ tetris/View.js
@@ -29,37 +29,38 @@ async function draw(channel, board, player_1, player_2, msg){
         return await channel.send(str);
     }
    else{
-        let str = ('|');
-        console.log(board)
+        let str = ('');
+        
         for(var i = 0; i < board.length; i++){
+            str = str.concat('|')
             for(var j = 0; j < board[i].length; j++){
-                switch (board[i][j]) {
+                switch (board[i][j]) { 
                     case 0:
-                        str = str.concat('     ')
+                        str = str.concat('       ')
                         break;
                     case 1:
-                        str = str.concat(' :red_square: ')
+                        str = str.concat(' :red_square:')
                         break;
                     case 2:
-                        str = str.concat(' :orange_square: ')
+                        str = str.concat(' :orange_square:')
                         break;
                     case 3:
-                        str = str.concat(' :yellow_square: ')
+                        str = str.concat(' :yellow_square:')
                         break;
                     case 4:
-                        str = str.concat(' :green_square: ')
+                        str = str.concat(' :green_square:')
                         break;
                     case 5:
-                        str = str.concat(' :blue_square: ')
+                        str = str.concat(' :blue_square:')
                         break;
                     case 6:
-                        str = str.concat(' :purple_square: ')
+                        str = str.concat(' :purple_square:')
                         break;    
                 }
             }
             str = str.concat('|\n')
         }
-        str = str.concat('---------------------------------------\n\n' +
+        str = str.concat('-------------------------------------------\n\n' +
         ':arrow_left:                                                  :arrow_right:');
         msg.edit(str);
     }
